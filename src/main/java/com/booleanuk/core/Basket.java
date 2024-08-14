896e8a9801b3f9fb03366a8b0d46e0bb788339ee
ansta1994 <anton.stahlgren@hotmail.com>
2024-08-14 14:51:09 +0200

    Fixed the add method, test Green

--- src/main/java/com/booleanuk/core/Basket.java
+++ src/main/java/com/booleanuk/core/Basket.java
@@ -1,5 +1,19 @@
 package com.booleanuk.core;
 
+import java.util.ArrayList;
+
 public class Basket {
 
+    ArrayList<String> basket;
+    int maxCapBasket;
+
+    public Basket() {
+        this.basket = new ArrayList<>();
+        this.maxCapBasket = 5;
+    }
+
+    public boolean add(String product) {
+        basket.add(product);
+        return basket.contains(product);
+    }
 }
